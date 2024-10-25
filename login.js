8c49be1433d5128c3658a0f15e1559b9f244350e
DiegoCastroT <diegodct6@gmail.com>
2024-10-25 19:43:25 +0200

    fixed problems

--- login.js
+++ login.js
@@ -1,15 +1,18 @@
-const toggleLinks = document.querySelectorAll('.toggle-link');
-const loginForm = document.getElementById('loginForm');
-const registerForm = document.getElementById('registerForm');
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleLinks = document.querySelectorAll('.toggle-link');
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
 
-toggleLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-        if (loginForm.style.display !== 'none') {
-            loginForm.style.display = 'none';
-            registerForm.style.display = 'block';
-        } else {
-            loginForm.style.display = 'block';
-            registerForm.style.display = 'none';
-         }
+    toggleLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Previene la acción predeterminada del enlace
+            if (loginForm.style.display !== 'none') {
+                loginForm.style.display = 'none';
+                registerForm.style.display = 'block';
+            } else {
+                loginForm.style.display = 'block';
+                registerForm.style.display = 'none';
+            }
+        });
     });
 });
